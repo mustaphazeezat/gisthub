d49718327227281ea8aa73d36450c3d6b5e2f511
mustaphazeezat <azeezat@softcom.ng>
2022-01-04 16:39:48 +0100

    fixed theme

--- src/components/wrapppers/Wrapper.js
+++ src/components/wrapppers/Wrapper.js
@@ -3,23 +3,17 @@ import { useTheme } from '../../context/ThemeContext'
 
 const Wrapper = ({children}) => {
     const darkTheme = useTheme()
-    const [theme, setTheme] = useState(true)
-    const getTheme = JSON.parse(localStorage.getItem("theme"));
+    const [theme, setTheme] = useState();
     useEffect(() => {
-        
-        if(getTheme === null){
-            setTheme(darkTheme)
-        }else if( getTheme === theme){
-            setTheme(getTheme)
-        }else if( getTheme !== theme){
-            setTheme(getTheme)
-        }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        setTheme(darkTheme)
     }, [])
     useEffect(() => {
-        setTheme(getTheme)
-        
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        const getTheme = JSON.parse(localStorage.getItem("theme"));
+        if (getTheme !== null) {
+            setTheme(getTheme)
+        }else{
+            setTheme(!theme)
+        }
     }, [darkTheme])
     return (
         <section className={`main-wrapper-y small-y main-wrapper-x main-wrapper ${theme? 'dark-main-wrapper' : 'light-main-wrapper'}`}>
